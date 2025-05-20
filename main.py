ffc662ee91d9d9102c4020b1cd3d8b794da17e83
pulkitavaid <pvaid2208@gmail.com>
2025-05-20 14:06:35 +0530

    Adding the transaction serviec

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI # type: ignore
 from routes import transaction
 from database import Base, engine
 
--- routes/transaction.py
+++ routes/transaction.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import SessionLocal
 import models, schemas
+import requests
 
 router = APIRouter()
 
@@ -14,6 +15,35 @@ def get_db():
 
 @router.post("/transactions/", response_model=schemas.TransactionOut)
 def create_transaction(txn: schemas.TransactionCreate, db: Session = Depends(get_db)):
+    # Fetch account details from account-service
+    account_url = f"http://account-service:8000/accounts/{txn.account_id}"
+    response = requests.get(account_url)
+
+    if response.status_code != 200:
+        raise HTTPException(status_code=404, detail="Account not found")
+
+    account = response.json()
+    current_balance = account["balance"]
+
+    # Debit check
+    if txn.type == "debit" and txn.amount > current_balance:
+        raise HTTPException(status_code=400, detail="Insufficient funds")
+
+    # Update account balance
+    if txn.type == "credit":
+        new_balance = current_balance + txn.amount
+    elif txn.type == "debit":
+        new_balance = current_balance - txn.amount
+    else:
+        raise HTTPException(status_code=400, detail="Invalid transaction type")
+
+    # Call PATCH to update balance
+    update_url = f"http://account-service:8000/accounts/{txn.account_id}/balance"
+    update_response = requests.patch(update_url, json={"balance": new_balance})
+    if update_response.status_code != 200:
+        raise HTTPException(status_code=500, detail="Failed to update balance")
+
+    # Store transaction
     db_txn = models.Transaction(**txn.dict())
     db.add(db_txn)
     db.commit()
